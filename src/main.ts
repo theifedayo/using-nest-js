907db1f7645a6a425bcb6f129fe2906cdbacea6e
theifedayo <adesiyanifedayo@gmail.com>
2023-02-09 18:28:29 +0100

    Register api prefix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api/v1')
   const configService = app.get(ConfigService);
 
   const enableCors = configService.get<boolean>('ENABLE_CORS');
